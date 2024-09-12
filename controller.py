1a60e0aff2df775de4a89d145985ecca3d613e59
PVL06 <pverl06@gmail.com>
2024-09-12 22:46:50 +0200

    sort players by lastname, create tournament and save

--- controller.py
+++ controller.py
@@ -1,12 +1,12 @@
 from pydantic import ValidationError
 
 from view import View
-from models import Player, PlayersRegister, Tournament
+from models import Player, PlayersDb, Tournament, TournamentsDb, PlayerPoint, Round
 
 class CtlPlayers:
     def __init__(self) -> None:
         self.view = View()
-        self.players_register = PlayersRegister()
+        self.players_db = PlayersDb()
 
     def register_new_player(self):
         user_input = self.view.new_player()
@@ -14,42 +14,44 @@ class CtlPlayers:
             player = Player(**user_input)
         except ValidationError as e:
             field_error = e.errors()[0].get("loc")[0]
-            self.viewer.view_error_message(f"Error in field '{field_error}'")
+            self.view.view_error_message(f"Error in field '{field_error}'")
         else:
-            if self.players_register.save_player(player):
-                self.viewer.view_message(f"New player id '{player.id}' saved !")
+            if self.players_db.save_new_player(player):
+                self.view.view_message(f"New player id '{player.id}' saved !")
             else:
-                self.viewer.view_error_message(f"Player id '{player.id}' already exist !")
+                self.view.view_error_message(f"Player id '{player.id}' already exist !")
 
-    #todo: trier par ordre alphabetique
     def register(self):
-        players = self.players_register.get_players()
+        players = self.players_db.get_all_players()
         list_of_players = [player.__dict__ for player in players]
-        self.viewer.view_table(list_of_players, "Registered players")
+        list_of_players.sort(key=lambda player: player["lastname"])
+        self.view.view_table(list_of_players, "Registered players")
 
 
 class CtlTournament:
     def __init__(self) -> None:
         self.view = View()
-        self.tournament = None
+        self.tournament_db = TournamentsDb()
 
-    def new_tournament(self):
-        self.create_new_tournament()
-        #self.add_players()
+    def all_tournaments(self):
+        tournaments = self.tournament_db.get_all_tournaments()
+        print(tournaments)
+
 
     def create_new_tournament(self):
-        
         run = True
         while run:
             user_input = self.view.create_tournament()
             rounds = user_input.get("number_of_round")
             if rounds.isnumeric() or not rounds:
                 rounds = int(rounds) if rounds else 4
-                if 4 <= rounds <= 10:
+                if 4 <= rounds <= 10:   
                     user_input["number_of_round"] = rounds
+                    print(user_input)
+                    self.tournament_db.save(Tournament(**user_input))
                     self.view.view_message(f"New tournament '{user_input.get('name')}' created !")
                     run = False
-                    self.tournament = Tournament(**user_input)
+
                 else:
                     self.view.view_error_message("Round must be in range 4 to 10 !")
             else:
@@ -76,9 +78,9 @@ class Controller:
             elif choice == "2":
                 self.players.register()
             elif choice == "3":
-                self.tournament.new_tournament()
+                self.tournament.create_new_tournament()
             elif choice == "4":
-                print('all tournament')
+                self.tournament.all_tournaments()
             elif choice == "5":
                 run = False
 
