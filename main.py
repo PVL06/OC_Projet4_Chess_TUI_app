b66709d080521648b3aebcf8a819f2f762ed8487
PVL06 <pverl06@gmail.com>
2024-09-24 21:06:06 +0200

    create folder data before name==main

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 
 from views.view_menu import Controller
 
+if not os.path.exists("data"):
+    os.mkdir("data")
 
 if __name__ == "__main__":
-    if not os.path.exists("data"):
-        os.mkdir("data")
     ctl = Controller()
     ctl.main_run()
